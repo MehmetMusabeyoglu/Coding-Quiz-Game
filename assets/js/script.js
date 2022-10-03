f57988dd7acb046118db48c45faac00210f4397b
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-10-03 01:25:04 -0800

    Edited logic for checking answer correctness.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,40 @@
 var timerEl = document.querySelector("#countdown");
 var questionEl = document.querySelector("#question");
 var optionsEl = document.querySelector("#options");
-
+var buttonEl = document.querySelectorAll("button");
+console.log(buttonEl);
 
 var timeLeft = 90;
-
+var questionIndex = 0;
 
 var quizQuestion1 = {
     question: "Question1 ",
     options: ["option A ", "option B", "option C", "option D"],
-    correctAnswer: this.options[2]
+    correctAnswer: 2
 };
 
 var quizQuestion2 = {
     question: "Question2",
     options: ["option A ", "option B", "option C", "option D"],
-    correctAnswer: this.options[2]
+    correctAnswer: 3
 };
 
 var quizQuestion3 = {
     question: "Question3",
     options: ["option A ", "option B", "option C", "option D"],
-    correctAnswer: this.options[2]
+    correctAnswer: 0
 };
 
 var quizQuestion4 = {
     question: "Question4",
     options: ["option A ", "option B", "option C", "option D"],
-    correctAnswer: this.options[2]
+    correctAnswer: 1
 };
 
 var quizQuestion5 = {
     question: "Question5",
     options: ["option A ", "option B", "option C", "option D"],
-    correctAnswer: this.options[2]
+    correctAnswer: 2
 };
 
 var questionList = [quizQuestion1, quizQuestion2, quizQuestion3, quizQuestion4, quizQuestion5];
@@ -43,17 +44,32 @@ function renderQuestion() {
 
     var questionList = JSON.parse(localStorage.getItem("questionListStringify"));
 
-    questionEl.textContent = questionList[3].question;
+    questionEl.textContent = questionList[questionIndex].question;
 
     for (var i = 0; i < 4; i++) {
 
         optionsEl.children[i].childNodes[2].textContent = questionList[1].options[i];
+        optionsEl.children[i].childNodes[1].addEventListener("click", function (event) {
+            var clickedButton = event.target;
+            console.log(clickedButton);
+            console.log(optionsEl.children[questionList[questionIndex].correctAnswer].childNodes[1]);
+            if(clickedButton === optionsEl.children[questionList[questionIndex].correctAnswer].childNodes[1]){
+                timeLeft=timeLeft+15;
+                console.log("equal to button");
+            }
+            else{
+                timeLeft=timeLeft-15;
+                console.log("not equal to button");
+            }
+            questionIndex++;
+            renderQuestion();
+            // console.log(element);
+            // console.log("I was clicked");
+        });
     }
 }
 
-
 renderQuestion();
-
 
 function countdown() {
     var timeInterval = setInterval(function () {
