6afa3c519dd6f236c6e108e3dd61501cff321015
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-10-02 23:44:37 -0800

    Added renderquestion function in js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,30 +6,49 @@ var optionsEl = document.querySelector("#options");
 var timeLeft = 90;
 
 
+var quizQuestion = {
+    question: "Choose A B C D ",
+    options: ["option A ", "option B", "option C", "option D"],
+    correctAnswer: this.options[2]
+};
 
+function renderQuestion() {
+
+    var questionList = JSON.parse(localStorage.getItem("questionListStringify"));
+
+    questionEl.textContent = quizQuestion.question;
+
+for (var i=0; i < 4; i++){
+
+    optionsEl.children[i].childNodes[2].textContent = quizQuestion.options[i];
+}
+}
+
+
+renderQuestion();
 
 
 function countdown() {
     var timeInterval = setInterval(function () {
-      if (timeLeft > 1) {
-        
-        timerEl.textContent = timeLeft + ' seconds remaining';
-        timeLeft--;
+        if (timeLeft > 1) {
 
-      } else if (timeLeft === 1) {
-        timerEl.textContent = timeLeft + ' second remaining';
-        timeLeft--;
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
 
-      } else {
-        timerEl.textContent = '';
-    
+        } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
 
-        clearInterval(timeInterval);
-  
+        } else {
+            timerEl.textContent = '';
 
-        displayResult();
-      }
+
+            clearInterval(timeInterval);
+
+
+            displayResult();
+        }
     }, 1000);
-  }
+}
 
-  countdown();
+countdown();
