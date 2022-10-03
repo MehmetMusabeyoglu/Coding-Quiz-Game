fafa7a18db51ea3c1b79c5fad0c6115100598adb
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-10-03 00:00:18 -0800

    Created question array and stored/retrieved w/JSON

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,49 @@ var optionsEl = document.querySelector("#options");
 var timeLeft = 90;
 
 
-var quizQuestion = {
-    question: "Choose A B C D ",
+var quizQuestion1 = {
+    question: "Question1 ",
     options: ["option A ", "option B", "option C", "option D"],
     correctAnswer: this.options[2]
 };
+
+var quizQuestion2 = {
+    question: "Question2",
+    options: ["option A ", "option B", "option C", "option D"],
+    correctAnswer: this.options[2]
+};
+
+var quizQuestion3 = {
+    question: "Question3",
+    options: ["option A ", "option B", "option C", "option D"],
+    correctAnswer: this.options[2]
+};
+
+var quizQuestion4 = {
+    question: "Question4",
+    options: ["option A ", "option B", "option C", "option D"],
+    correctAnswer: this.options[2]
+};
+
+var quizQuestion5 = {
+    question: "Question5",
+    options: ["option A ", "option B", "option C", "option D"],
+    correctAnswer: this.options[2]
+};
+
+var questionList = [quizQuestion1, quizQuestion2, quizQuestion3, quizQuestion4, quizQuestion5];
+localStorage.setItem("questionListStringify", JSON.stringify(questionList));
 
 function renderQuestion() {
 
     var questionList = JSON.parse(localStorage.getItem("questionListStringify"));
 
-    questionEl.textContent = quizQuestion.question;
+    questionEl.textContent = questionList[3].question;
 
-for (var i=0; i < 4; i++){
+    for (var i = 0; i < 4; i++) {
 
-    optionsEl.children[i].childNodes[2].textContent = quizQuestion.options[i];
-}
+        optionsEl.children[i].childNodes[2].textContent = questionList[1].options[i];
+    }
 }
 
 
