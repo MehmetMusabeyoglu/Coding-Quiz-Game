100388272d2c5345da7ec809990fb2b3168c5dac
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-10-03 16:59:38 -0800

    Edited style.css and added score function in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,102 @@
 var timerEl = document.querySelector("#countdown");
 var questionEl = document.querySelector("#question");
 var optionsEl = document.querySelector("#options");
-var buttonEl = document.querySelectorAll("button");
-console.log(buttonEl);
+// var buttonEl = document.querySelectorAll("button");
+// console.log(buttonEl);
 
 var timeLeft = 90;
 var questionIndex = 0;
+//var userClicked = false;
 
 var quizQuestion1 = {
     question: "Question1 ",
-    options: ["option A ", "option B", "option C", "option D"],
-    correctAnswer: 2
+    options: ["1-A ", "1-B", "1-C", "1-D"],
+    correctAnswer: 0
 };
 
 var quizQuestion2 = {
     question: "Question2",
-    options: ["option A ", "option B", "option C", "option D"],
-    correctAnswer: 3
+    options: ["2-A ", "2-B", "2-C", "2-D"],
+    correctAnswer: 0
 };
 
 var quizQuestion3 = {
     question: "Question3",
-    options: ["option A ", "option B", "option C", "option D"],
+    options: ["3-A ", "3-B", "3-C", "3-D"],
     correctAnswer: 0
 };
 
 var quizQuestion4 = {
     question: "Question4",
-    options: ["option A ", "option B", "option C", "option D"],
-    correctAnswer: 1
+    options: ["4-A ", "4-B", "4-C", "4-D"],
+    correctAnswer: 0
 };
 
 var quizQuestion5 = {
     question: "Question5",
-    options: ["option A ", "option B", "option C", "option D"],
-    correctAnswer: 2
+    options: ["5-A ", "5-B", "5-C", "5-D"],
+    correctAnswer: 0
 };
 
 var questionList = [quizQuestion1, quizQuestion2, quizQuestion3, quizQuestion4, quizQuestion5];
-localStorage.setItem("questionListStringify", JSON.stringify(questionList));
+//localStorage.setItem("questionListStringify", JSON.stringify(questionList));
+
+function listenForAnswer(event) {
+    var clickedButton = event.target;
+    console.log(clickedButton);
+    console.log(optionsEl.children[questionList[questionIndex].correctAnswer].childNodes[1]);
+    if (clickedButton === optionsEl.children[questionList[questionIndex].correctAnswer].childNodes[1]) {
+        timeLeft = timeLeft + 15;
+        console.log("correct");
+    }
+    else {
+        timeLeft = timeLeft - 15;
+        console.log("wrong");
+    }
+
+    
+    if(questionIndex < questionList.length){
+        questionIndex++;
+        renderQuestion();
+    }else{
+        scoreSubmission();
+    }
+   
+    // console.log(element);
+    // console.log("I was clicked");
+}
 
 function renderQuestion() {
-
-    var questionList = JSON.parse(localStorage.getItem("questionListStringify"));
-
+    //   var questionList = JSON.parse(localStorage.getItem("questionListStringify"));
     questionEl.textContent = questionList[questionIndex].question;
-
     for (var i = 0; i < 4; i++) {
-
-        optionsEl.children[i].childNodes[2].textContent = questionList[1].options[i];
-        optionsEl.children[i].childNodes[1].addEventListener("click", function (event) {
-            var clickedButton = event.target;
-            console.log(clickedButton);
-            console.log(optionsEl.children[questionList[questionIndex].correctAnswer].childNodes[1]);
-            if(clickedButton === optionsEl.children[questionList[questionIndex].correctAnswer].childNodes[1]){
-                timeLeft=timeLeft+15;
-                console.log("equal to button");
-            }
-            else{
-                timeLeft=timeLeft-15;
-                console.log("not equal to button");
-            }
-            questionIndex++;
-            renderQuestion();
-            // console.log(element);
-            // console.log("I was clicked");
-        });
+        optionsEl.children[i].childNodes[2].textContent = questionList[questionIndex].options[i];
+        optionsEl.children[i].childNodes[1].removeEventListener("click", listenForAnswer);
+        optionsEl.children[i].childNodes[1].addEventListener("click", listenForAnswer);
     }
 }
+
+// function listenForAnswer(questionIndex){
+//     for (var i = 0; i < 4; i++) {
+//             optionsEl.children[i].childNodes[1].addEventListener("click", function (event) {
+//             var clickedButton = event.target;
+//             //console.log(clickedButton);
+//             //console.log(optionsEl.children[questionList[questionIndex].correctAnswer].childNodes[1]);
+//             if(clickedButton === optionsEl.children[questionList[questionIndex].correctAnswer].childNodes[1]){
+//                 timeLeft=timeLeft+15;
+//                 console.log("correct");
+//             }
+//             else{
+//                 timeLeft=timeLeft-15;
+//                 console.log("wrong");
+//             }
+//             userClicked = True;
+//             // console.log(element);
+//             // console.log("I was clicked");
+//         });
+//     }
+// }
+
 
 renderQuestion();
 
@@ -85,9 +114,7 @@ function countdown() {
         } else {
             timerEl.textContent = '';
 
-
             clearInterval(timeInterval);
-
 
             displayResult();
         }
@@ -95,3 +122,12 @@ function countdown() {
 }
 
 countdown();
+
+// for(var questionIndex=0; questionIndex<5; questionIndex++){
+//     renderQuestion(questionIndex);
+//     listenForAnswer(questionIndex);
+//     // while(!userClicked){
+
+//     // }
+//     // userClicked = false;
+// }
